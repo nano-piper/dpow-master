9a43f26132f0a5d7e3e4b5942b35aa672a816a13
Robin Thomas <robinthomas2591@gmail.com>
2020-04-12 10:54:34 +0800

    react + express setup

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.json({ success: "ok" });
+});
+
 app.get("/pow/:blockHash", (req, res) => {
 
 });
